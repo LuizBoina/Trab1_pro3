24a0f620ceeedef432750406934397de5ba52a97
Fernando Bisi Vieira <32550535+fernando0x40@users.noreply.github.com>
2018-05-03 07:45:43 -0300

    métodos de Leitura passados para Main

--- trabalho/src/trabalho/Main.java
+++ trabalho/src/trabalho/Main.java
@@ -1,11 +1,46 @@
 package trabalho;
 
+import java.util.Objects;
+
 public class Main {
-	
+	private static final int N_PLANILHAS = 6;
+	private static boolean soLeitura = false;
+	private static boolean soEscrita = false;
+
 	public static void main(String[] args) {
-		Leitura input = new Leitura();
-		input.lerLinhaComando(args);
-
+		String[] planilhas = Main.lerLinhaComando(args);
+		Main.imprimirNomesArquivos(planilhas);
 	}
-
+	
+	public static String[] lerLinhaComando(String[] linhaDeComando) {
+		String[] arquivos = new String[N_PLANILHAS];
+		int length = linhaDeComando.length;
+		for(int i = 0; i < length ;i++) {
+			if(Objects.equals(linhaDeComando[i], "-d"))
+				arquivos[0] = linhaDeComando[++i];
+			else if(Objects.equals(linhaDeComando[i], "-a"))
+				arquivos[1] = linhaDeComando[++i];
+			else if(Objects.equals(linhaDeComando[i], "-p"))
+				arquivos[2] = linhaDeComando[++i];
+			else if(Objects.equals(linhaDeComando[i], "-c"))
+				arquivos[3] = linhaDeComando[++i];
+			else if(Objects.equals(linhaDeComando[i], "-r"))
+				arquivos[4] = linhaDeComando[++i];
+			else if(Objects.equals(linhaDeComando[i], "-o"))
+				arquivos[5] = linhaDeComando[++i];
+			else if(Objects.equals(linhaDeComando[i], "NAOESPECIFICADO"))
+				arquivos[6] = linhaDeComando[++i];
+			else if(Objects.equals(linhaDeComando[i], "--read-only"))
+				soLeitura = true;
+			else if(Objects.equals(linhaDeComando[i], "--write-only"))
+				soEscrita = true;
+		}
+		//Main.imprimirNomesArquivos(arquivos);
+		return arquivos;
+	}
+	public static void imprimirNomesArquivos(String[] arquivos) {
+		int length = arquivos.length;
+		for(int i = 0 ; i < length ; i++) 
+			System.out.println(arquivos[i]);
+	}
 }
