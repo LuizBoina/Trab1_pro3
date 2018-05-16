30220e6cca97bf51c1a9b9f92b2d31462c2761df
proibido <lipeboina.boina@hotmail.com>
2018-05-16 16:54:18 -0300

    rmv trab errado

--- trabalho/src/trabalho/Curso.java
+++
@@ -1,9 +0,0 @@
-package trabalho;
-
-public class Curso {
-	private int codigo;
-	private String nome;
-	private boolean grad;
-	private boolean posGrad;
-	// vetor disciplinas
-}
--- trabalho/src/trabalho/DiscenteGrad.java
+++
@@ -1,5 +0,0 @@
-package trabalho;
-
-public class DiscenteGrad extends Discente{
-
-}
